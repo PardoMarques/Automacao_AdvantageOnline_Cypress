6d2769daca45f0f45f146553fdf2051994dd6b0d
Caio Marques <caio_asm@hotmail.com>
2024-09-16 11:28:36 -0300

    Update user.request.js

--- cypress/requests/user.request.js
+++ cypress/requests/user.request.js
@@ -8,30 +8,6 @@ export default class UserRequest {
         this.userPostAutenticarUsuario(usuario);
     }
 
-    // static userPostCadastrarUsuario(email, firstName, loginName, password, userRole) {
-    //     return cy.fixture('users/postCadastrarUsuario.json').then((jsonBody) => {
-    //         // Arrange
-    //         jsonBody.accountType = userRole;
-    //         jsonBody.email = email;
-    //         jsonBody.firstName = firstName;
-    //         jsonBody.loginName = loginName;
-    //         jsonBody.password = password;
-
-    //         // Act
-    //         cy.request({
-    //             method: 'POST',
-    //             url: '/accountservice/accountrest/api/v1/register',
-    //             headers: { 'content-type': 'application/json' },
-    //             body: jsonBody
-    //         }).then((response) => {
-    //             Cypress.env("currentResponseStatus", response.status);
-    //             Cypress.env("currentResponseBody", response.body.response);
-    //             Cypress.env("currentUserId", response.body.userId);
-    //             return response
-    //         });
-    //     });
-    // }
-
     static userPostCadastrarUsuario(usuario) {
         var userId;
         // Arrange
@@ -82,57 +58,27 @@ export default class UserRequest {
             Cypress.env("currentToken", `Bearer ${currentToken}`);
         });
     }
-
-    // static userPostAutenticarUsuario(email, loginPassword, loginUser){
-    //     return cy.fixture('users/postAutenticarUsuario.json').then((jsonBody) => {
-    //         // Arrange
-    //         jsonBody.email = email;
-    //         jsonBody.loginPassword = loginPassword;
-    //         jsonBody.loginUser = loginUser;
-
-    //         // Act
-    //         cy.request({
-    //             method: 'POST',
-    //             url: '/accountservice/accountrest/api/v1/login',
-    //             headers: { 'content-type': 'application/json' },
-    //             body: jsonBody
-    //         }).then((response) => {
-    //             expect(response.body).to.have.property('statusMessage');
-    //             Cypress.env("currentResponseStatus", response.status);
-    //             Cypress.env("currentResponseBody", response.body);
-    //             Cypress.env("currentToken", response.body.statusMessage.token);
-    //             return response
-    //         });
-    //         cy.then(() => {
-    //             const valorArmazenado = Cypress.env('currentResponseBody');
-
-    //             // [Sincronizar] Verifica se o valor foi armazenado corretamente
-    //             expect(valorArmazenado).to.not.be.undefined;
-    //             cy.log('Usando o valor armazenado: ' + valorArmazenado);
-    //         });
-    //     });
-    // }
     
-    static userDeleteApagarUsuario(bearerToken, accountId){
-        return cy.fixture('users/deleteApagarUsuario.json').then((jsonBody) => {
-            // Arrange
-            jsonBody.accountId = accountId;
+    static userDeleteApagarUsuario(accountId){
+        // Arrange
+        const jsonBody = require('../fixtures/users/deleteApagarUsuario.json');
+        jsonBody.accountId = accountId;
 
-            // Act
-            cy.request({
-                method: 'DELETE',
-                url: '/accountservice/accountrest/api/v1/delete',
-                headers: { 
-                    'content-type': 'application/json',
-                    'authorization': Cypress.env("currentToken")
-                },
-                body: jsonBody
-            }).then((response) => {
-                Cypress.env("currentResponseStatus", response.status);
-                Cypress.env("currentResponseBody", response.status);
-                return response
-            });
+        // Act
+        cy.request({
+            method: 'DELETE',
+            url: '/accountservice/accountrest/api/v1/delete',
+            headers: { 
+                'content-type': 'application/json',
+                'authorization': Cypress.env("currentToken")
+            },
+            body: jsonBody
+        }).then((response) => {
+            Cypress.env("currentResponseStatus", response.status);
+            Cypress.env("currentResponseBody", response.status);
+            return response
         });
+        
     }
 
 }
